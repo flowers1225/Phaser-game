c1aec14f9d90eaf6404430906e839751e7e80628
flowers1225 <438052770@qq.com>
2017-06-05 17:44:05 +0800

    优化

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,6 +15,8 @@ var phaser = path.join(phaserModule, 'build/custom/phaser-split.js'),
     pixi = path.join(phaserModule, 'build/custom/pixi.js'),
     p2 = path.join(phaserModule, 'build/custom/p2.js');
 
+var zepto = path.join(__dirname, 'src/js/lib/zepto.min.js');
+
 module.exports = function () {
     return {
         entry: {
@@ -43,7 +45,7 @@ module.exports = function () {
         },
         resolve: {
             alias: {
-                'zepto': './lib/zepto.min.js',
+                'zepto': zepto,
                 'phaser': phaser,
                 'pixi': pixi,
                 'p2': p2
